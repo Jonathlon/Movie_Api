dd64556c4cf32804003ea2a5a419742013e00a52
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-03-18 22:27:10 +0000

    added REST and API endpoints
    Including test file and documentation

--- index.js
+++ index.js
@@ -1,101 +1,232 @@
-const express = require('express');
- morgan = require ('morgan');
- fs = require ('fs'),
- path = require ('path');
+const express = require("express"),
+  app = express(),
+  morgan = require("morgan"),
+  fs = require("fs"),
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 
-const app = express();
+app.use(bodyParser.json());
 
-
-// Middleware - Morgan function (data logging)
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
-
-app.use(morgan('combined', {stream: accessLogStream}));
-
-app.use('/', express.static('public'));
-
-app.get('/', (req, res) => {
-  res.send('Welcome to my Movie app!');
-});
-
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
-});
-
-//List of top movies
-let topMovies = [
+let users = [
   {
-    title: 'Sicario',
-    director: 'Denis Villeneuve',
-    'top cast': 'Emily Blunt, Josh Brolin, Benicio Del Toro',
+    id: 1,
+    name: "Jon",
+    favoriteMovies: [],
   },
   {
-    title: 'The Dark Knight',
-    director: 'Christopher Nolan',
-    'top cast': 'Christian Bale, Heath Ledger',
+    id: 2,
+    name: "Lucy",
+    favoriteMovies: ["Jurassic Park"],
   },
-  {
-    title: 'Fight Club',
-    director: 'David Fincher',
-    'top cast': 'Brad Pitt, Edward Norton',
-  },
-  {
-    title: 'Pulp Fiction',
-    director: 'Quentin Tarantino',
-    'top cast': 'John Travolta, Uma Thurman, Samuel L. Jackson',
-  },
-
-  {
-    title: 'Interstellar',
-    director: 'Christopher Nolan',
-    'top cast': 'Matthew McConaughey, Anne Hathaway, Jessica Chastain',
-  },
-  {
-    title: 'The Departed',
-    director: 'Martin Scorsese',
-    'top cast': 'Leonardo DiCaprio, Matt Damon, Jack Nicholson',
-  },
-  {
-    title: 'Good Will Hunting',
-    director: 'Gus Van Sant',
-    'top cast': 'Matt Damon, Ben Affleck, Robin Williams',
-  },
-  {
-    title: 'Die Hard',
-    director: 'John McTiernan',
-    'top cast': 'Bruce Willis, Alan Rickman, Bonnie Bedelia',
-  },
-  {
-    title: 'Snatch',
-    director: 'Guy Ritchie',
-    'top cast': 'Jason Statham, Brad Pitt, Stephen Graham',
-  },
-  {
-    title: 'Jurassic Park',
-    director: 'Steven Spielberg',
-    'top cast': 'Sam Neill, Laura Derrn, Jeff Goldblum',
-  },
-
-
 ];
 
-// GET requests
+let movies = [
+  {
+    Title: "The Dark Knight",
+    Description:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    Genre: {
+      Name: "Action",
+      Description:
+        "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.",
+    },
+    Director: {
+      Name: "Christopher Nolan",
+      Bio: "Best known for his cerebral, often nonlinear, storytelling, acclaimed writer-director Christopher Nolan was born on July 30, 1970, in London, England. Over the course of 15 years of filmmaking, Nolan has gone from low-budget independent films to working on some of the biggest blockbusters ever made.",
+      Birth: "1970",
+    },
+    ImageURL:
+      "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/91ebheNmoUL._RI_.jpg",
+    Featured: false,
+  },
+  {
+    Title: "Jurassic Park",
+    Description:
+      "A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.",
+    Genre: {
+      Name: "Adventure",
+      Description:
+        "An adventure film is a form of adventure fiction, and is a genre of film. Subgenres of adventure films include swashbuckler films, pirate films, and survival films. Adventure films may also be combined with other film genres such as action, comedy, drama, fantasy, science fiction, family, horror, war, or the medium of animation.",
+    },
+    Director: {
+      Name: "Steven Spielberg",
+      Bio: "One of the most influential personalities in the history of cinema, Steven Spielberg is Hollywood's best known director and one of the wealthiest filmmakers in the world. He has an extraordinary number of commercially successful and critically acclaimed credits to his name, either as a director, producer or writer since launching the summer blockbuster with Jaws (1975), and he has done more to define popular film-making since the mid-1970s than anyone else.",
+      Birth: "1946",
+    },
+    ImageURL:
+      "https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_.jpg",
+    Featured: false,
+  },
+  {
+    Title: "The Departed",
+    Description:
+      "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.",
+    Genre: {
+      Name: "Drama",
+      Description:
+        "In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.",
+    },
+    Director: {
+      Name: "Martin Scorsese",
+      Bio: "Martin Charles Scorsese was born on November 17, 1942 in Queens, New York City, to Catherine Scorsese (née Cappa) and Charles Scorsese, who both worked in Manhattan's garment district, and whose families both came from Palermo, Sicily. He was raised in the neighborhood of Little Italy, which later provided the inspiration for several of his films. Scorsese earned a B.S. degree in film communications in 1964, followed by an M.A. in the same field in 1966 at New York University's School of Film. During this time, he made numerous prize-winning short films including The Big Shave (1967), and directed his first feature film, Who's That Knocking at My Door (1967).",
+      Birth: "1942",
+    },
+    ImageURL:
+      "https://www.comingsoon.net/wp-content/uploads/sites/3/2021/10/Departed-e1633146591556.jpg?w=640",
+    Featured: false,
+  },
+];
 
+//CREATE
+app.post("/users", (req, res) => {
+  const newUser = req.body;
 
-app.get('/documentation', (req, res) => {                  
-  res.sendFile('public/documentation.html', { root: __dirname });
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("users need name");
+  }
 });
 
-app.get('/movies', (req, res) => {
-  res.json(topMovies);
+//UPDATE
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//CREATE
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//DELETE
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(
+      (title) => title !== movieTitle
+    );
+    res
+      .status(200)
+      .send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//DELETE
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    users = users.filter((user) => user.id != id);
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//READ
+app.get("/movies", (req, res) => {
+  res.status(200).json(movies);
+});
+
+//READ
+app.get("/movies/:title", (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find((movie) => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("No such movie");
+  }
+});
+
+//READ
+app.get("/movies/genre/:genreName", (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("No such genre");
+  }
+});
+
+//READ
+app.get("/movies/directors/:directorName", (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find(
+    (movie) => movie.Director.Name === directorName
+  ).Director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("No such Director");
+  }
+});
+
+// READ Documentation
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", { root: __dirname });
+});
+
+// Middleware - Morgan function (data logging)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+  flags: "a",
+});
+
+app.use(morgan("combined", { stream: accessLogStream }));
+
+app.use("/", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my Movie app!");
+});
+
+app.get("/secreturl", (req, res) => {
+  res.send("This is a secret url with super top-secret content.");
 });
 
 //Middleware error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
 // listen for requests
-app.listen(8080, () =>{
-  console.log('Your app is listening on port 8080.');
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
 });
