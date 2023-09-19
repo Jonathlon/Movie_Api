cad684113a068815afa271cf2f25b1856ac1b02b
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-09-19 18:02:25 +0100

    added cors origin

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require("cors");
 let allowedOrigins = [
   "http://localhost:8080",
+  "http://localhost:4200",
   "http://testsite.com",
   "http://localhost:1234",
   "https://jonathlonmyflix.netlify.app",
