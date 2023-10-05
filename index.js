b4833dd4b17db8ee03532d2841ceb0ccee582e51
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-10-05 16:42:12 +0100

    updated index.js

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ app.put(
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    let hashedPassword = Users.hashPassword(req.body.password);
+    let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
